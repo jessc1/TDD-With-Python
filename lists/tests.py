8d35c069f49747dd26e97b8f0ea7e298dee5f234
jessc1 <mmariajessica@gmail.com>
2020-05-19 16:01:17 -0300

    functional and unitttests

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,14 @@
+from django.urls import resolve
 from django.test import TestCase
-from django.test import TestCase
+from lists.views import home_page
+
+class HomePageTests(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
+
 
 # Create your tests here.
-class SmokeTest(TestCase):
+"""class SmokeTest(TestCase):
     def test_bad_maths(self):
-        self.assertEqual(1+ 1, 3)
+        self.assertEqual(1+ 1, 3)"""
